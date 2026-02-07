0ff920228086fe46c8991b0f3e7fce9e65aa8dfa
Alex <alexis.kueny@outlook.com>
2026-02-07 14:52:23 +0100

    Initial commit

---
+++ battlemaster-app/src/_components/header.tsx
@@ -0,0 +1,11 @@
+import { Header } from "antd/es/layout/layout";
+
+export default function AppHeader() {
+    return (
+        <Header
+            color="--bg-light"
+        >
+            Header element
+        </Header>
+    )
+}
